5e7e4f2ebb10cbae5e7178b4a4899c2def3da9df
ashwinipule <ashwinipule00@example.com>
2024-05-25 09:56:49 -0400

    Modified IMDB scraping script

--- Assignment_may_8th/Imdb_scraping.py
+++ Assignment_may_8th/Imdb_scraping.py
@@ -22,16 +22,19 @@ try:
     movies = soup.find('ul', class_="ipc-metadata-list ipc-metadata-list--dividers-between sc-a1e81754-0 eBRbsI compact-list-view ipc-metadata-list--base").find_all('li')
     result = []
     for movie in movies:
-        name = movie.find('div' , class_="ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-b189961a-9 iALATN cli-title").a.text
-        rank = movie.find('span', class_="ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating")
+        second_div = movie.select_one('div:nth-of-type(2)')
+        second_div.find('h3').text
+        #name = movie.find('div' , class_="ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-b189961a-9 iALATN cli-title").a.text
+        #rank = movie.find('span', class_="ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating")
         # rank.attrs['aria-label']
-        year = movie.find('span', class_="sc-b189961a-8 kLaxqf cli-title-metadata-item").text
-        result.append((name,rank.attrs['aria-label'],year))
-        print(result)
-        sheet.append([name,rank.attrs['aria-label'],year])
+        #year = movie.find('span', class_="sc-b189961a-8 kLaxqf cli-title-metadata-item").text
+        #result.append((name,rank.attrs['aria-label'],year))
+        #print(result)
+        #sheet.append([name,rank.attrs['aria-label'],year])
+        break
 
 except Exception as e:
     print(e) 
 
-excel.save('IMDB_MoviewRating.xlsx')
+#excel.save('IMDB_MoviewRating.xlsx')
 
